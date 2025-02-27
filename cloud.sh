0abfe1338ea6bc5c16d0d1ef92d8238e4a8d9515
IamtherealBrian <2638932112@qq.com>
2025-02-27 12:26:58 -0500

    set up the pythonanywhere url for the cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Run your React frontend against a local API server.
-export REACT_APP_URL_PRE="https://<YOUR_SERVERS_CLOUD_ADDRESS_HERE>/"
+export REACT_APP_URL_PRE="https://brianwu.pythonanywhere.com/"
 npm start
+
