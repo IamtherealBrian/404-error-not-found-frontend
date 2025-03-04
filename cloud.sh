f5597d82a3cc33f40915fad1cb436b09baf1d886
Hyan <95455834+Haomingyan@users.noreply.github.com>
2025-03-04 12:54:53 -0500

    run frontend on cloud server

--- cloud.sh
+++ cloud.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Run your React frontend against a local API server.
-export REACT_APP_URL_PRE="https://<YOUR_SERVERS_CLOUD_ADDRESS_HERE>/"
+# If you want to use a cloud server, replace the URL below with your cloud server address
+export REACT_APP_URL_PRE="http://localhost:8000/"
 npm start
