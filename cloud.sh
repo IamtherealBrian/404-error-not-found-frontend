606cffc26fac6791777bec5d23c900ef097afa2a
Hyan <95455834+Haomingyan@users.noreply.github.com>
2025-03-04 14:45:12 -0500

    clean up the code

--- cloud.sh
+++ cloud.sh
@@ -2,6 +2,5 @@
 # Run your React frontend against a local API server.
 # Using PythonAnywhere backend
 export REACT_APP_URL_PRE="https://brianwu.pythonanywhere.com"
-export NODE_ENV=development
 npm start
 
